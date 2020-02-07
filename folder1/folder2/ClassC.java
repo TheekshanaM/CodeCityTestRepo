18e2c7efb683301479a0dd12e8022266048ee6ad
TheekshanaM <93.madushan@gmail.com>
2020-02-07 14:30:35 +0530

    add classC

---
+++ folder1/folder2/ClassC.java
@@ -0,0 +1,11 @@
+package folder1.folder2;
+
+class ClassB {
+    public void QMethod(String args[]) {
+        int x = 7;
+        System.out.println("Hello");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+    }
+}
