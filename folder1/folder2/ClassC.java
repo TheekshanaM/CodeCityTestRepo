2d5bab7672c3596d29ad948aa24fb98a308f6d4f
TheekshanaM <93.madushan@gmail.com>
2020-02-07 14:32:06 +0530

    update classC

--- folder1/folder2/ClassC.java
+++ folder1/folder2/ClassC.java
@@ -4,8 +4,10 @@ class ClassB {
     public void QMethod(String args[]) {
         int x = 7;
         System.out.println("Hello");
-        System.out.println("1");
+        System.out.println("1.1");
+        System.out.println("1.2");
         System.out.println("2");
+        System.out.println("2.1");
         System.out.println("3");
     }
 }
