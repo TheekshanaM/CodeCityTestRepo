52751d689d368f0f3ae9e8a34ecc1b24a9a9bcce
TheekshanaM <93.madushan@gmail.com>
2020-02-07 14:35:18 +0530

    change prinstatment in classC

--- folder1/folder2/ClassC.java
+++ folder1/folder2/ClassC.java
@@ -4,10 +4,12 @@ class ClassB {
     public void QMethod(String args[]) {
         int x = 7;
         System.out.println("Hello");
-        System.out.println("1.1");
+        System.out.println("1.1.1");
+        System.out.println("1.1.2");
         System.out.println("1.2");
         System.out.println("2");
-        System.out.println("2.1");
+        System.out.println("2.1.1");
+        System.out.println("1.1.2");
         System.out.println("3");
     }
 }
